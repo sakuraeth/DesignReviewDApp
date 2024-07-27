abf8b4b85b6ca117899039068668a7a8a27a155a
sakuraeth <163349116+sakuraeth@users.noreply.github.com>
2024-07-27 21:11:04 +0300

    change variable

--- contracts/DesignReview.sol
+++ contracts/DesignReview.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.8.0;
 contract DesignReviewDApp {
 
     event DesignUploaded(uint256 designId, address indexed uploader);
-    event ReviewAdded(uint256 designId, address indexed reviewer, string review);
-    event DesignVoted(uint256 designId, address indexed voter, uint256 votes);
+    event ReviewAdded(uint256 designId, address indexed reviewer, string reviewText);
+    event DesignVoted(uint256 designId, address indexed voter, uint256 totalVotes);
 
     struct Design {
         uint256 id;
         address uploader;
-        string dataURI;
+        string designDataURL;
         uint256 totalVotes;
     }
 
@@ -19,35 +19,35 @@ contract DesignReviewDApp {
         string reviewText;
     }
 
-    uint256 private designCounter;
+    uint256 private totalDesigns;
     mapping(uint256 => Design) private designs;
     mapping(uint256 => Review[]) private designReviews;
-    mapping(uint256 => mapping(address => bool)) private designVoted;
+    mapping(uint256 => mapping(address => bool)) private hasVotedForDesign;
 
-    function uploadDesign(string memory _dataURI) external {
-        designCounter += 1;
-        designs[designCounter] = Design(designCounter, msg.sender, _dataURI, 0);
-        emit DesignUploaded(designCounter, msg.sender);
+    function submitDesign(string memory designDataURL) external {
+        totalDesigns += 1;
+        designs[totalDesigns] = Design(totalDesigns, msg.sender, designDataURL, 0);
+        emit DesignUploaded(totalDesigns, msg.sender);
     }
 
-    function leaveReview(uint256 _designId, string memory _reviewText) external {
-        require(_designId > 0 && _designId <= designCounter, "Design does not exist.");
-        designReviews[_designId].push(Review(_designId, msg.sender, _reviewText));
-        emit ReviewAdded(_designId, msg.sender, _reviewText);
+    function postReview(uint256 designId, string memory reviewText) external {
+        require(designId > 0 && designId <= totalDesigns, "Design does not exist.");
+        designReviews[designId].push(Review(designId, msg.sender, reviewText));
+        emit ReviewAdded(designId, msg.sender, reviewText);
     }
     
-    function voteForDesign(uint256 _designId) external {
-        require(_designId > 0 && _designId <= designCounter, "Design does not exist.");
-        require(!designVoted[_designId][msg.sender], "You have already voted for this design.");
+    function castVoteForDesign(uint256 designId) external {
+        require(designId > 0 && designId <= totalDesigns, "Design does not exist.");
+        require(!hasVotedForDesign[designId][msg.sender], "You have already voted for this design.");
         
-        designs[_designId].totalVotes += 1;
-        designVoted[_designId][msg.sender] = true;
-        emit DesignVoted(_designId, msg.sender, designs[_designId].totalVotes);
+        designs[designId].totalVotes += 1;
+        hasVotedForDesign[designId][msg.sender] = true;
+        emit DesignVoted(designId, msg.sender, designs[designId].totalVotes);
     }
 
-    function getDesignDetails(uint256 _designId) external view 
+    function getDesignDetails(uint256 designId) external view 
     returns (Design memory design, Review[] memory reviews) {
-        require(_designId > 0 && _designId <= designCounter, "Design does not exist.");
-        return (designs[_designId], designReviews[_designId]);
+        require(designId > 0 && designId <= totalDesigns, "Design does not exist.");
+        return (designs[designId], designReviews[designId]);
     }
 }
